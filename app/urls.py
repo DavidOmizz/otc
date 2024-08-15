2c949d05a45dedacf5e8d4a1a88b22c1c593015c
David Omisakin <davidomisakin4good@gmail.com>
2024-08-15 12:14:29 +0100

    done

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('api/cohort/', CohortInfoListCreate.as_view(), name='cohort-list-create'),
+    path('', CohortInfoListCreate.as_view(), name='cohort-list-create'),
     path('api/cohort/<int:pk>/', CohortInfoDetail.as_view(), name='cohort-detail'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
