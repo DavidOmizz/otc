6e3a3dd5b983875ef3f3fc17ddb3727535c168b9
David Omisakin <davidomisakin4good@gmail.com>
2024-08-15 12:03:50 +0100

    Another commit

--- otcform/settings.py
+++ otcform/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hmb2jdumo3jtdl2vym(a+&ch4g_yd$_6wjy*x=qo(_aa&gvikh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','.vercel.app']
 
 
 # Application definition
