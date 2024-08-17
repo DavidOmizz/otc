22a3a97e0f7b6b8c71396ea64f0adba2d245b6ab
David Omisakin <71450801+DavidOmizz@users.noreply.github.com>
2024-08-17 12:27:01 +0100

    Update settings.py

--- otcform/settings.py
+++ otcform/settings.py
@@ -122,6 +122,13 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
